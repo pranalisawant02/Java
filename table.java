b71a3aecca2ae3feb5ca5281ac0949ef43bdebd9
Pranali Sawant <pranalisawant1403@gmail.com>
2025-06-24 12:03:49 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,17 @@
+package com.dkte;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=2;
+		for(int i=1;i<=10;i++)
+		{
+			int res=i*n;
+			System.out.println(res);
+			
+		}
+		System.out.println(" ");
+	}
+
+}

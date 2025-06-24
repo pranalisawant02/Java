2938126d58f335c49d51fbc00e6f5a019c4c600a
Pranali Sawant <pranalisawant1403@gmail.com>
2025-06-24 12:01:10 +0530

    Create MaxNum.java

---
+++ MaxNum.java
@@ -0,0 +1,29 @@
+package com.dkte;
+
+public class MaxNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=6;
+		int c=7;
+		
+		if(a>b) {
+			if(a>c)
+			{
+				System.out.println("a is maximum");
+			}
+		}
+		else if(b>c)
+		{
+			System.out.println("b is maximum");
+		}
+		else
+		{
+			System.out.println("c is maximum");
+		}
+		
+
+	}
+
+}

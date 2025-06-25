51c7a2ef9539370f8ac42dab0341aa17344fe2de
Pranali Sawant <pranalisawant1403@gmail.com>
2025-06-25 12:07:26 +0530

    Create area_rect.java

---
+++ area_rect.java
@@ -0,0 +1,15 @@
+package demo2;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double length=10.0, breadth=5.0,area;
+		
+		area=length*breadth;
+		
+		System.out.println("Area:"+area);
+		System.out.printf("Length:%2f , Breadth:%2f",length,breadth);
+	}
+
+}
